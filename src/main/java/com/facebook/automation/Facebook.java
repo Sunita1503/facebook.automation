eb6162ce12981df543d5f7ad8146310670d44528
Your Name <sunita@vervesquare.com>
2020-11-05 21:44:20 +0530

    Facebook code

--- src/main/java/com/facebook/automation/Facebook.java
+++ src/main/java/com/facebook/automation/Facebook.java
@@ -14,114 +14,111 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class Facebook {
-	
-	WebDriver driver  = null;
-	
-	//set up initial environment
-	@BeforeSuite
-	public void setup(){
-	
-		//set driver exe path
-	System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "\\src\\main\\resources\\drivers\\chromedriver.exe");
-	
-	driver = new ChromeDriver();
-	
-	//maximize window
-	driver.manage().window().maximize();
-	
-	//open url
-	driver.get("https://www.facebook.com/r.php");
-	
-	//implicit wait
-	driver.manage().timeouts().implicitlyWait(15,TimeUnit.SECONDS);
-	
+
+	public static WebDriver driver = null;
+
+	// set up initial environment
+	@Test(priority = 1)
+	public void setup() {
+
+		// set driver exe path
+		System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\main\\resources\\drivers\\chromedriver.exe");
+
+		driver = new ChromeDriver();
+
+		// maximize window
+		driver.manage().window().maximize();
+
+		// open url
+		driver.get("https://www.facebook.com/r.php");
+
+		// implicit wait
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+
 	}
-	
-	//Register for new facebook user using valid first name , last name  and password 
-	@Test (priority = 1)
-	public void registration(){
-		
-		//Explicit wait
-		/*
-		WebDriverWait objWebDriverWait = new WebDriverWait(driver, 10);
-		objWebDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@name='firstname']")));
-		*/
-		
-		
+
+	// Register for new facebook user using valid first name , last name and
+	// password
+	@Test(priority = 2)
+	public void registration() {
+
+		// locate and set firstname
 		WebElement userName = driver.findElement(By.xpath("//input[@name='firstname']"));
 		userName.sendKeys("Sunita");
-		
-		//System.out.println("*******Get Attribute of name is : " + userName.getAttribute("name"));
-		
+
+		// locate and set lastname
 		driver.findElement(By.name("lastname")).sendKeys("shinde");
-		driver.findElement(By.name("reg_email__")).sendKeys("100200323");
-		driver.findElement(By.id("password_step_input")).sendKeys("afaf");
-		
+
+		// locate and set random 10 digit mobile number
+		driver.findElement(By.name("reg_email__")).sendKeys(generateMobileNo());
+		// locate and set password
+		driver.findElement(By.id("password_step_input")).sendKeys("abcdfgh");
+
 	}
-	@Test (priority = -2)
-	public void enterDay(){
-				
+
+	// Generate random mobile No.
+	public static String generateMobileNo() {
+
+		long mobileNo = (long) Math.floor(Math.random() * 9999999999L);
+
+		// convert long value to string
+		String strMobile = String.valueOf(mobileNo);
+		String mobileNumber = strMobile.replaceFirst("(\\d{3})(\\d{3})(\\d+)", "($1) $2-$3");
+
+		System.out.println(mobileNumber);
+
+		return mobileNumber;
+
+	}
+
+	@Test(priority = 3)
+	public void enterDay() {
+
 		WebElement day = driver.findElement(By.name("birthday_day"));
-		
-		Select objSelect= new Select(day);
-		
+
+		Select objSelect = new Select(day);
+
 		objSelect.selectByIndex(3);
-		//objSelect.selectByValue("8");
-		//objSelect.selectByVisibleText("14");
-		
+
 	}
-	
-	//enter month
-	@Test (priority = -1)
-	public void enterMonth(){
-		//locate month
+
+	// enter month
+	@Test(priority = 3)
+	public void enterMonth() {
+		// locate month
 		WebElement monthLocator = driver.findElement(By.id("month"));
-		
+
 		Select objSelectMonth = new Select(monthLocator);
-		
+
 		List<WebElement> allOption = objSelectMonth.getOptions();
-		
-		int size = allOption.size();
-		System.out.println("Total months are : " + size);
-		
-		String month;
-		for (int i=1; i<size; i++){
-			month = allOption.get(i).getText();
-			System.out.println(month);			
-		}
+
 		allOption.get(5).click();
-			
+
 	}
-	
-	//Enter Year
-	@Test (priority = 0)
-	public void enterYear(){
-		
+
+	// Enter Year
+	@Test(priority = 4)
+	public void enterYear() {
+
 		List<WebElement> yearData = driver.findElements(By.xpath("//option[contains(@value,'20')]"));
-		//year.sendKeys("2012");	
-		int size = yearData.size();
-		
-		String birthYear ;
-		
-		for (int i=0; i<size; i++){
-		birthYear = yearData.get(i).getText();
-		System.out.println(birthYear);
-		}
+
 		yearData.get(4).click();
-		
+
 	}
-	
-	//Select Radio button
-	@Test
-	public void selectRadioButton(){
-		
-		//locate radio button Male and click
-		WebElement radioBtn = driver.findElement(By.xpath("//input[@name ='sex' and contains(@value,'2') ]"));
-		radioBtn.click();
-		
-		//locate Sign Up button and click 
-		driver.findElement(By.name("websubmit")).click();
+
+	// Select Radio button
+	@Test(priority = 5)
+	public void selectRadioButton() {
+
+		// locate radio button Male and click
+		driver.findElement(By.xpath("//input[@name ='sex' and contains(@value,'2') ]")).click();
+		;
+		// driver.findElement(By.id("u_0_5")).click();
+		// radioBtn.click();
+
+		// locate Sign Up button and click
+		// driver.findElement(By.name("websubmit")).click();
 	}
-	
 
 }
